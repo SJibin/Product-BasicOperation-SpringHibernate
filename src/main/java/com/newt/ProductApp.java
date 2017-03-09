d1cb618a3d0bd484f2d16742acc472d509ca7cb0
S Jibin <sjibin93@gmail.com>
2017-03-09 14:08:26 +0530

    Fixed the error faced to show in the swagger UI.

--- src/main/java/com/newt/ProductApp.java
+++ src/main/java/com/newt/ProductApp.java
@@ -3,18 +3,18 @@ package com.newt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableEurekaClient
+//@EnableEurekaClient     // If we need to Register with Eureka Server then use this anotation.
 @EnableSwagger2
-public class ProductMain {
+public class ProductApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductMain.class, args);
+		SpringApplication.run(ProductApp.class, args);
+		
+		
 	}
-
+	 
 }
